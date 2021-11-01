69d2ea84d1ec1b154948e33d0631e5ef2b6f99e3
AnthonyBarceloRosales <anthonybarcelorosales@gmail.com>
2021-11-01 11:59:24 -0700

    commit to pulling dev

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from './components/Navbar/index';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => {
+  <div>
+    hey
+  </div>
+}
+
+
+export default Navbar
