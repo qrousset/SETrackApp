449626c1f6eede0813147178e83cdeae250b47d5
Q <roussetquent1@gmail.com>
2021-11-02 11:13:57 -0400

    Added Cards component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import SummaryCard from "../components/SummaryCard";
 
 const Home = () => {
+  let testArr = [];
+
   return (
     <div id="home">
       <h1>Home</h1>
+      <SummaryCard />
     </div>
   );
 };
