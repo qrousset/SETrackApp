0a9af84d80e1f00fce50d4b654e84c62e3ffe658
Q <roussetquent1@gmail.com>
2021-11-03 09:52:15 -0400

    added Listing button and all fields

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -16,17 +16,24 @@ export default function BasicCard(props) {
         <Typography variant="h5" component="div">
           {props.data.application_name || "Placeholder"}
         </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {props.data.job_listing}
+        <Typography sx={{ mb: 1.0 }} color="text.secondary">
+          Comment on the listing
         </Typography>
         <div className="steps">
-          <Typography variant="body2"> sent </Typography>
+          <Typography variant="body2"> Submission date: </Typography>
           <Typography variant="body2">{props.data.submission_date}</Typography>
+          <Typography variant="body2">Current step:</Typography>
           <Typography variant="body2"> {props.data.status_name} </Typography>
-          <Typography variant="body2"> {props.data.next_status} </Typography>
+          <Typography variant="body2">Next step:</Typography>
+          <Typography variant="body2">{props.data.next_status}</Typography>
+          <Typography variant="body2">Next step date:</Typography>
+          <Typography variant="body2">{props.data.date}</Typography>
         </div>
       </CardContent>
-      <CardActions>
+      <CardActions style={{ justifyContent: "space-around" }}>
+        <Button size="small" href={props.data.job_listing}>
+          Listing
+        </Button>
         <Button size="small">Update</Button>
       </CardActions>
     </Card>
